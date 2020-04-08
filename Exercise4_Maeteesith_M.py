e8be9f37fa00cfb9bc5e9b3be68a04496313c56c
Jr095837 <jr095837@gmail.com>
2020-04-08 10:59:51 +0700

    Exercise 4 : Exam scores
    print exam score

---
+++ Exercise4_Maeteesith_M.py
@@ -0,0 +1,11 @@
+FE = 17.5           #Foundation English
+GB = 19             #General Business
+ItCS = 19.5         #Introduction to Computer Systems
+CP = 20             #Computer Programming
+full = 20           #Full Scores
+print("========== Your Scores ==========")
+print("Foundation English :", FE, "/", full)
+print("General Business :", GB, "/", full)
+print("Introduction to Computer Systems :", ItCS, "/", full)
+print("Computer Programming :", CP, "/", full)
+
